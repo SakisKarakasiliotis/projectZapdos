48c35f283e16f3af5cceb0e08a696b75ed86bc90
MChatzid <markos.chatzidimitriou@gmail.com>
2016-11-08 18:10:05 +0200

    Implemented bucket class for hashClass
    Resolves false positives and equal hash codes

--- my_hash.cpp
+++ my_hash.cpp
@@ -3,11 +3,10 @@
 #include <stdio.h>
 #include "my_hash.h"
 
+//-------------HASH CLASS-------------
 hashClass::hashClass(int initialSize, short int precision)
 {
-   this->table = (char**) malloc(initialSize*sizeof(char*));
-   for (int i=0; i<initialSize; i++)
-      sprintf(this->table[i], "AA"); //We know that the input will be like "A <num> <num>"
+   this->table = new bucket[initialSize];
    this->precision = precision;
 }
 
@@ -16,16 +15,48 @@ hashClass::~hashClass()
    delete this->table;
 }
 
-uint32_t hashClass::hashFun(char* input)
+uint32_t hashClass::hashFun(const char* input)
 {
    return (*(uint32_t*) input)>> precision;
 }
 
-bool hashClass::check_hash(char* input)
+bool hashClass::check_hash(const char* input)
 {
    uint32_t bucket = hashFun(input);
-   if (!strcmp(this->table[bucket], input))
+   if (this->table[bucket].existsInBucket(input))
       return true;
-   sprintf(this->table[bucket], "%s", input);
+   return false;
+}
+
+//-------------BUCKET CLASS-------------
+bucket::bucket()
+{
+   this->tableSize = 2;
+   this->bucketTable = (char**) malloc(tableSize*sizeof(char*));
+   this->numOfElements = 0;
+}
+
+bucket::~bucket()
+{
+   free(this->bucketTable);
+}
+
+void bucket::insert(const char* input)
+{
+   if (this->numOfElements == this->tableSize)
+   {
+      this->tableSize *= 2;
+      this->bucketTable = (char**) realloc(bucketTable, this->tableSize*sizeof(char*)); 
+   }
+   sprintf(this->bucketTable[numOfElements], "%s", input);
+   this->numOfElements++;
+}
+
+bool bucket::existsInBucket(const char* input)
+{
+   for (int i=0; i<this->numOfElements; i++)
+      if (!strcmp(bucketTable[i], input))
+         return true;
+   this->insert(input);
    return false;
 }
--- my_hash.h
+++ my_hash.h
@@ -3,17 +3,29 @@
 
 #include "defines.h"
 
+class bucket 
+{
+   private:
+      char** bucketTable;
+      int tableSize;
+      int numOfElements;
+      void insert(const char*);
+   public:
+      bucket();
+      ~bucket();
+      bool existsInBucket(const char*);
+};
+
 class hashClass
 {
    private:
-
-      char** table;
+      bucket* table;
       short int precision;
-      uint32_t hashFun(char*);
+      uint32_t hashFun(const char*);
    public:
       hashClass(int, short int);
       ~hashClass();
-      bool check_hash(char*);
+      bool check_hash(const char*);
 };
 
 #endif
