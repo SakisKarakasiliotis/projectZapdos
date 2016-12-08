ea96764990d9bc4e899bb5513c1e604b11207452
mpampoulas <kimsempire@hotmail.com>
2016-12-08 17:27:03 +0200

    eftia3a kapws thn main
    still needs to concider the static case though!!!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-#include <sstream>
 #include <fstream>
-#include <string>
-#include <cstdlib>
 #include <cstring>
 
 #include "defines.h"
@@ -20,101 +17,114 @@ int main(int argc, char const *argv[])
 {
 
 
-	 //FILE READING VARIABLES
+//FILE READING VARIABLES -----------------------------------------------------------------------------------------------
    string inpout;
    char *part1,part2[80],part3[80],initial[80],get[600];
    ifstream inpoutFile;
    string in;
    char inpoutLine[80]={0};
-   //GRAPH VARIABLES
+//GRAPH VARIABLES ------------------------------------------------------------------------------------------------------
    buffer* Buffer ;
    n_index* Index ;
    buffer* Buffer_inv ;
    n_index* Index_inv ;
-   //HELPERS
-   int entry, lineNumber=0, OPTION=2,entry_inv, j=0;
+//HELPERS --------------------------------------------------------------------------------------------------------------
+   int entry=0, lineNumber=0, OPTION=2,entry_inv=0, numberOfQuestions=0;
 
-
-   //START READING FILES FOR GRAPH CREATION
-   cout<<"INSERT ENTRY FILE: ";
+//START READING FILES FOR GRAPH CREATION--------------------------------------------------------------------------------
+   cout<<"INSERT ENTRY FILE: "<<endl;
    do{
-        //TODO FIX THE FUCKING PATHS dUM dUM
+        //TODO: SELECT PATH ACCORDING TO FILE REQUIRED !!!!!!!
         if(OPTION==1){
         	strcpy(get,"C:\\Users\\Windows 8\\projectZapdosClion\\tinyWorkload_FINAL.txt");
         }
         else{
-//        	strcpy(get,"C:\\Users\\Windows 8\\projectZapdosClion\\tinyGraph.txt");
 //        	strcpy(get,"C:\\Users\\Windows 8\\projectZapdosClion\\a.txt");
 //        	strcpy(get,"C:\\Users\\Windows 8\\projectZapdosClion\\b.txt");
 //        	strcpy(get,"C:\\Users\\Windows 8\\Desktop\\tiny\\tinyGraph.txt");
-        	strcpy(get,"C:\\Users\\Windows 8\\Desktop\\small\\smallGraph.txt");
+//        	strcpy(get,"C:\\Users\\Windows 8\\Desktop\\small\\smallGraph.txt");
         }
 
-
+// Initializing FILE getting first token--------------------------------------------------------------------------------
        inpoutFile.open(get);
-
        getline(inpoutFile,inpout);
        strcpy(inpoutLine,inpout.c_str());
        part1= strtok(inpoutLine," \n\t");
        cout<<"FIRST LINE "<<lineNumber<<" TOKEN: "<<part1<<endl;
+
+// Token selected check for case Addition/Question or Creation----------------------------------------------------------
+
        if(!strcmp(part1,"A")||!strcmp(part1,"Q")){
+// -------------!! Workload execution started !!!----------------------------------------------------------------------
          cout<<"WORK FILE"<<endl;
          while(!strcmp(part1,"A")||!strcmp(part1,"Q")){
            if(strcmp(part1,"F")){
+
              strcpy(initial,part1);
              part1=strtok(NULL," \n\t");
              strcpy(part2,part1);
-
              part1=strtok(NULL," \n\t");
              strcpy(part3,part1);
+// Case A adding entry to Graph-----------------------------------------------------------------------------------------
              if(!strcmp(initial,"A")){
-               cout<< "ADDING TO GRAPH" <<endl;
-              entry = Index->getEntry(atoi(part2));
-              if(entry == FAIL){
-                 cout<<"OUT OF BOUNDS"<<endl;
-              }else if(entry == INVALID){
-                 int temp = Buffer->addListNode();
-                 Index->addEntry(temp,atoi(part2));
-                 Buffer->insertNeighbor(Index->getEntry(atoi(part2)),atoi(part3));
+                 cout<< "ADDING TO GRAPH" <<endl;
+// Updating outgoing Index ---------------------------------------------------------------------------------------------
+                 entry = Index->getEntry(atoi(part2));
+                  if(entry == FAIL){
+                     cout<<"OUT OF BOUNDS"<<endl;
+                  }
+                  else if(entry == INVALID){
+                     int temp = Buffer->addListNode();
+                     Index->addEntry(temp,atoi(part2));
+                     Buffer->insertNeighbor(Index->getEntry(atoi(part2)),atoi(part3));
+                  }
+                  else if(entry!=INVALID){
+                     Buffer->insertNeighbor(Index->getEntry(atoi(part2)),atoi(part3));
+                  }
 
-
-              }else if(entry!=INVALID){
-                 Buffer->insertNeighbor(Index->getEntry(atoi(part2)),atoi(part3));
-
-              }
-              entry_inv = Index_inv->getEntry(atoi(part3));
-              if(entry_inv == FAIL){
-                 cout<<"OUT OF BOUNDS"<<endl;
-              }else if(entry_inv == INVALID){
-                 int temp_inv = Buffer_inv->addListNode();
-                 Index_inv->addEntry(temp_inv,atoi(part3));
-                 Buffer_inv->insertNeighbor(Index_inv->getEntry(atoi(part3)),atoi(part2));
-                 //
-
-              }else if(entry_inv!=INVALID){
-                 Buffer_inv->insertNeighbor(Index_inv->getEntry(atoi(part3)),atoi(part2));
-              }
-
-
+// Updating incoming Index ---------------------------------------------------------------------------------------------
+                  entry_inv = Index_inv->getEntry(atoi(part3));
+                  if(entry_inv == FAIL){
+                     cout<<"OUT OF BOUNDS"<<endl;
+                  }
+                  else if(entry_inv == INVALID){
+                     int temp_inv = Buffer_inv->addListNode();
+                     Index_inv->addEntry(temp_inv,atoi(part3));
+                     Buffer_inv->insertNeighbor(Index_inv->getEntry(atoi(part3)),atoi(part2));
+                  }
+                  else if(entry_inv!=INVALID){
+                     Buffer_inv->insertNeighbor(Index_inv->getEntry(atoi(part3)),atoi(part2));
+                  }
+// End of case A -------------------------------------------------------------------------------------------------------
              }
+
+// Case Q for question on the graph ------------------------------------------------------------------------------------
              else if(!strcmp(initial,"Q")){
                cout<<"Question"<<endl;
-               cout<<j<<"-------------"<<BBFS(Index, Buffer, atoi(part2), Index_inv, Buffer_inv, atoi(part3), 50)<<" this is the fucking answer"<<endl;
-             	j++;
+               cout<<numberOfQuestions<<"------BBFS-------"<<BBFS(Index, Buffer, atoi(part2), Index_inv, Buffer_inv, atoi(part3), 50)<<" this is the answer"<<endl;
+                 numberOfQuestions++;
              }
+// End of case Q -------------------------------------------------------------------------------------------------------
           }
-           getline(inpoutFile,inpout);
 
+// Getting new line from Workload FILE ---------------------------------------------------------------------------------
+           getline(inpoutFile,inpout);
            strcpy(inpoutLine,inpout.c_str());
            part1=strtok(inpoutLine," \n\t");
+// End of while statement starting next run-----------------------------------------------------------------------------
          }
-       }else{
+// ----!!! Workload Finish !!!------------------------------------------------------------------------------------------
+       }
+// -------!!! Graph Creation starts here !!!!!--------------------------------------------------------------------------
+       else{
          cout<< "GRAPH CREATION"<<endl;
+// Initializing Graph structs ------------------------------------------------------------------------------------------
          Buffer = new buffer(BUFFER_SIZE,LIST_NODE_SIZE);
          Index = new n_index(INDEX_SIZE);
          Buffer_inv = new buffer(BUFFER_SIZE,LIST_NODE_SIZE);
          Index_inv = new n_index(INDEX_SIZE);
 
+// Reading input from FILE - Insert data in Graph-----------------------------------------------------------------------
          while(strcmp(part1,"S")){
            strcpy(initial,part1);
            part1=strtok(NULL," \n\t");
@@ -124,38 +134,47 @@ int main(int argc, char const *argv[])
            		int temp = Buffer->addListNode();
            		Index->addEntry(temp,atoi(initial));
            }
-//             cout<<"Index size: "<<Index->getSizeOfIndex()<<endl;
-//             cout<<"Index NOE: "<<Index->getNumberOfEntries()<<endl;
-//            cout<<"initial "<<Index->getEntry(atoi(initial))<<" part2 "<<atoi(part2)<<endl;
-             //TODO: getEntry bullshit number
+
            Buffer->insertNeighbor(Index->getEntry(atoi(initial)),atoi(part2));
+
            if(Index_inv->getEntry(atoi(part2))==INVALID){
                       int temp_inv = Buffer_inv->addListNode();
                       Index_inv->addEntry(temp_inv,atoi(part2));
-                }
+           }
+
            Buffer_inv->insertNeighbor(Index_inv->getEntry(atoi(part2)),atoi(initial));
            getline(inpoutFile,inpout);
+
            if(!inpoutFile){
              break;
            }
 
            strcpy(inpoutLine,inpout.c_str());
            part1= strtok(inpoutLine," \n\t");
-             lineNumber++;
+           lineNumber++;
+// End of while statement -- read new line for Graph insertion ---------------------------------------------------------
          }
          inpoutFile.close();
+// End of Graph Insert -- Input FILE is closed--------------------------------------------------------------------------
+
+// GCC call and test prints --------------------------------------------------------------------------------------------
            cout<<"Before Main call"<<endl;
            GetConnectedComponents(Index, Buffer, Index_inv, Buffer_inv);
            cout<<"After Main call"<<endl;
-//           Index_inv->printCCnum();
-           Index->printCCnum();
-//          uint32_t* neighbors;
-//           int k;
-//          neighbors = Buffer->getNeighbors(k, Index->getEntry(1));
-//           for (int i = 0; i < (k); i++) {
-//               cout<<"neighbors of 0 "<<neighbors[i]<<endl;
-//           }
+
+//         Index_inv->printCCnum(); // print outgoing Index: nodename -- offset -- CCnumber !!!
+           Index->printCCnum();     // print incoming Index: nodename -- offset -- CCnumber !!!
+//         uint32_t* neighbors;
+//         int k;
+//         neighbors = Buffer->getNeighbors(k, Index->getEntry(1));
+//         for (int i = 0; i < (k); i++){
+//             cout<<"neighbors of 0 "<<neighbors[i]<<endl;
+//         }
+
+// End of test prints --------------------------------------------------------------------------------------------------
        }
+//---!!! Graph creation finished !!!!-----------------------------------------------------------------------------------
+
        cout<<"CHOOSE"<<endl<< "0 - QUIT"<<endl<<"OR"<<endl<< "1 - INSERT WORK FILE"<<endl;
        cin>>OPTION;
        if (OPTION == 0)
@@ -163,9 +182,8 @@ int main(int argc, char const *argv[])
          inpoutFile.close();
          cout << "EXITING" <<endl;
        }
-  }while( OPTION == 1 ); //KEEP READING UNTIL 0
-   //end from reading
+  }while( OPTION == 1 ); // KEEP READING WORKLOAD FILE UNTIL TERMINATED BY 0 INPUT ------------------------------------
 
-	cout<<"GG WP"<<endl;
+	cout<<"----- END OF MAIN()! -----"<<endl;
 	return 0;
 }
