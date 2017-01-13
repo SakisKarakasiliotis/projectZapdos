648c7969de716d74d7ebc50104dedea20eb4ee8c
MChatzid <markos.chatzidimitriou@gmail.com>
2017-01-13 02:35:38 +0200

    Better names

--- staticGraph.cpp
+++ staticGraph.cpp
@@ -40,14 +40,15 @@ int staticGraph() {
    do{
       //TODO: SELECT PATH ACCORDING TO FILE REQUIRED !!!!!!!
       if(OPTION==1){//WORKLOAD
-          strcpy(get,"C:\\Users\\user\\Desktop\\datasets\\small\\smallWorkload_FINAL.txt");
+         strcpy(get, "C:\\Users\\Marcus\\VirtualBox VMs\\linuxmint\\linux projects\\projectZapdos\\small\\smallWorkload_FINAL.txt");
+//          strcpy(get,"C:\\Users\\user\\Desktop\\datasets\\small\\smallWorkload_FINAL.txt");
 //          strcpy(get,"C:\\Users\\user\\Desktop\\datasets\\tiny\\tinyWorkload_FINAL.txt");
 //          strcpy(get,"C:\\Users\\Windows 8\\projectZapdosClion\\small\\smallWorkload_FINAL.txt");
       }
       else{//GRAPH
+         strcpy(get,"C:\\Users\\Marcus\\VirtualBox VMs\\linuxmint\\linux projects\\projectZapdos\\small\\smallGraph.txt");
 //          strcpy(get,"C:\\Users\\Windows 8\\projectZapdosClion\\tiny\\wikigraph.txt");
 //          strcpy(get,"C:\\Users\\Windows 8\\projectZapdosClion\\small\\smallGraph.txt");
-          strcpy(get,"C:\\Users\\user\\Desktop\\datasets\\small\\smallGraph.txt");
 //          strcpy(get,"C:\\Users\\user\\Desktop\\datasets\\tiny\\tinyGraph.txt");
       }
 
--- tarjan.cpp
+++ tarjan.cpp
@@ -50,111 +50,111 @@ OK_SUCCESS grail::calculateSCC(n_index *subGraphIndex, n_index *subGraphIndexInv
        std::cout << "Checking node " << i << std::endl;
        uint32_t* baseNeighbors = NULL;
        int baseNumberOfNeighbors;
-       tarjanNode *v = nodes + i;
-       while (idBelongsToComponent[i] == INVALID) {
-          int numberOfNeighbors;
-          uint32_t *neighbors;
-//        tarjanNode *v = nodes + i; !!!!!!!!!!!!!!!!den allazei mesa sthn while exei logo na einai mesa?
-          //DFSStack->push(v.nodeName);
-          //v.DFSInitialized = true;
+       tarjanNode *currentNode = nodes + i;
+      
+       if (idBelongsToComponent[i] != INVALID) continue;
+      
+       int numberOfNeighbors;
+       uint32_t *neighbors;
+//     tarjanNode *currentNode = nodes + i; !!!!!!!!!!!!!!!!den allazei mesa sthn while exei logo na einai mesa?
+       //DFSStack->push(currentNode.nodeName);
+       //currentNode.DFSInitialized = true;
 
-          int whileCount = 1;
-          do {
-              whileCount++;
-              if(v> nodes + x || v < nodes){
-                  cout<<"failure is imminent"<<endl;
-              }
-              if(v->nodeName == INVALID || v->nodeName == FAIL){
-                  cout << "the end is coming 3" << endl;
-              }
-              // Set the depth index for v to the smallest unused index
-              if (v->tIndex == INVALID) {
-                  v->tIndex = lowestIndex;
-                  v->lowlink = lowestIndex;
-                  lowestIndex++;
-                  TarjanStack->push(v->nodeName);
-                  //v->onTarjanStack = true;
-              }
+       int whileCount = 1;
+       do {
+           whileCount++;
+           if(currentNode> nodes + x || currentNode < nodes){
+               cout<<"failure is imminent"<<endl;
+           }
+           if(currentNode->nodeName == INVALID || currentNode->nodeName == FAIL){
+               cout << "the end is coming 3" << endl;
+           }
+           // Set the depth index for currentNode to the smallest unused index
+           if (currentNode->tIndex == INVALID) {
+               currentNode->tIndex = lowestIndex;
+               currentNode->lowlink = lowestIndex;
+               lowestIndex++;
+               TarjanStack->push(currentNode->nodeName);
+               //currentNode->onTarjanStack = true;
+           }
+            // Consider successors of currentNode
+           int offset = subGraphIndex->getEntry(currentNode->nodeName);
+           if (offset != INVALID) {
+//               if (currentNode->nodeName = i){
+//                 if (baseNeighbors == NULL) {
+//                     baseNeighbors = subGraphBuffer->getNeighbors(numberOfNeighbors, offset);
+//                     baseNumberOfNeighbors = numberOfNeighbors;
+//                 }
+//                 neighbors = baseNeighbors;
+//                 numberOfNeighbors = baseNumberOfNeighbors;
+//             }
+//             else {
+//                 neighbors = subGraphBuffer->getNeighbors(numberOfNeighbors, offset);
+//             }
+               neighbors = subGraphBuffer->getNeighbors(numberOfNeighbors, offset);
+               int j;
+               for (j = 0; j < numberOfNeighbors; j++) {
+                   //currentNode->neighborsChecked++;
+                   if (!nodes[neighbors[j]].DFSInitialized) {
+                       DFSStack->push(currentNode->nodeName);
+                       currentNode->DFSInitialized = true;
+                       currentNode = &nodes[neighbors[j]];
+                       if(currentNode->nodeName == INVALID || currentNode->nodeName == FAIL){
+                           cout << "the end is coming 2" << endl;
+                       }
+                       break;
+                   } else {
+                       if (idBelongsToComponent[neighbors[j]] == INVALID &&
+                           nodes[neighbors[j]].lowlink < currentNode->lowlink) {
+                           currentNode->lowlink = nodes[neighbors[j]].lowlink;
+                       }
+                       continue;
+                   }
+               }
+               if (j == numberOfNeighbors) {
+                   currentNode->DFSInitialized = true;
+                   int tmp = DFSStack->pop();
+                   if (tmp != INVALID){
+                       currentNode = &nodes[tmp];
+                   }
+                   if(currentNode> nodes + x || currentNode < nodes ){
+                       cout<<"failure is imminent 2"<<endl;
+                   }
+                   if(currentNode->nodeName == INVALID || currentNode->nodeName == FAIL){
+                       cout << "the end is coming" << endl;
+                   }
+               }
+           } else {
+               currentNode->DFSInitialized = true;
+               break;
+           }
+       } while (!DFSStack->isEmpty());
+       std::cout << "Exited while after loop No " << whileCount << std::endl;
+       //currentNode = &nodes[i];
 
-              // Consider successors of v
-              int offset = subGraphIndex->getEntry(v->nodeName);
-              if (offset != INVALID) {
-//                if (v->nodeName = i){
-//                    if (baseNeighbors == NULL) {
-//                        baseNeighbors = subGraphBuffer->getNeighbors(numberOfNeighbors, offset);
-//                        baseNumberOfNeighbors = numberOfNeighbors;
-//                    }
-//                    neighbors = baseNeighbors;
-//                    numberOfNeighbors = baseNumberOfNeighbors;
-//                }
-//                else {
-//                    neighbors = subGraphBuffer->getNeighbors(numberOfNeighbors, offset);
-//                }
-                  neighbors = subGraphBuffer->getNeighbors(numberOfNeighbors, offset);
-                  int j;
-                  for (j = 0; j < numberOfNeighbors; j++) {
-                      //v->neighborsChecked++;
-                      if (!nodes[neighbors[j]].DFSInitialized) {
-                          DFSStack->push(v->nodeName);
-                          v->DFSInitialized = true;
-                          v = &nodes[neighbors[j]];
-                          if(v->nodeName == INVALID || v->nodeName == FAIL){
-                              cout << "the end is coming 2" << endl;
-                          }
-                          break;
-                      } else {
-                          if (idBelongsToComponent[neighbors[j]] == INVALID &&
-                              nodes[neighbors[j]].lowlink < v->lowlink) {
-                              v->lowlink = nodes[neighbors[j]].lowlink;
-                          }
-                          continue;
-                      }
-                  }
-                  if (j == numberOfNeighbors) {
-                      v->DFSInitialized = true;
-                      int tmp = DFSStack->pop();
-                      if (tmp != INVALID){
-                          v = &nodes[tmp];
-                      }
-                      if(v> nodes + x || v < nodes ){
-                          cout<<"failure is imminent 2"<<endl;
-                      }
-                      if(v->nodeName == INVALID || v->nodeName == FAIL){
-                          cout << "the end is coming" << endl;
-                      }
-                  }
-              } else {
-                  v->DFSInitialized = true;
-                  break;
-              }
-          } while (!DFSStack->isEmpty());
-          std::cout << "Exited while after loop No " << whileCount << std::endl;
-          //v = &nodes[i];
-
-          // If v is a root node, pop the stack and generate an SCC
-          do {
-              component *a = new component();
-              a->setCompId(numberOfComponents);
-              int *commponentNodes = new int[TarjanStack->getNumberOfElements()];
-              int c = 0;
-              tarjanNode *w;
-              int nodeNo;
-              do {
-                  nodeNo = TarjanStack->pop();
-                  w = nodes + nodeNo;//TODO CHECK THIS THING
-                  //w->onTarjanStack = false;
-                  commponentNodes[c] = w->nodeName;
-                  c++;
-                  idBelongsToComponent[w->nodeName] = numberOfComponents;
-                  std::cout << nodeNo << " was assigned to SCC " << numberOfComponents << std::endl;
-              } while (w->tIndex != w->lowlink);
-              a->setNodesContained(commponentNodes);
-              a->setNumberOfNodes(c);
-              assignComponent(components, compArraySize, numberOfComponents, a);
-              //insert a into current SCC
-              numberOfComponents++;
-          }while (!TarjanStack->isEmpty()); // a wild do while appeared!!!!
-      }
+       // If currentNode is a root node, pop the stack and generate an SCC
+       do {
+           component *newComponent = new component();
+           newComponent->setCompId(numberOfComponents);
+           int *commponentNodes = new int[TarjanStack->getNumberOfElements()];
+           int c = 0;
+           tarjanNode *poppedNode;
+           int nodeNo;
+           do {
+               nodeNo = TarjanStack->pop();
+               poppedNode = nodes + nodeNo;//TODO CHECK THIS THING
+               //poppedNode->onTarjanStack = false;
+               commponentNodes[c] = poppedNode->nodeName;
+               c++;
+               idBelongsToComponent[poppedNode->nodeName] = numberOfComponents;
+               std::cout << nodeNo << " was assigned to SCC " << numberOfComponents << std::endl;
+           } while (poppedNode->tIndex != poppedNode->lowlink);
+           newComponent->setNodesContained(commponentNodes);
+           newComponent->setNumberOfNodes(c);
+           assignComponent(components, compArraySize, numberOfComponents, newComponent);
+           //insert a into current SCC
+           numberOfComponents++;
+       }while (!TarjanStack->isEmpty()); // a wild do while appeared!!!!
    }
 
    this->SCC = new stronglyConnectedComponents(components, numberOfComponents, idBelongsToComponent);
